e4161ed5b3f79fab803f87b988caa88f39b2b27b
muyong <whatissrc@gmail.com>
2019-04-18 11:48:57 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# vue 组件库
+
+[![Travis CI  Status](https://travis-ci.org/piaohan/vue-components.svg?branch=master)](https://travis-ci.org/piaohan/vue-components)
+
+---
+
+## 项目基于 vuecli3.\* + vuetify.js 构建的组件库
+
+步骤 1：
+
+```js
+npm i
+or
+yarn
+```
+
+步骤 2：
+
+```
+npm run serve
+```
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <ul>
+      <li v-for="(video,index) in videos" :key="index">
+        <VideoPlay :src="video.src" :type="video.type"/>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import VideoPlay from "@/components/VideoPlay.vue";
+
+export default {
+  name: "home",
+  components: {
+    VideoPlay
+  },
+  data() {
+    return {
+      videos: [
+        {
+          src: "rtmp://192.168.0.211:1935/live/leo",
+          type: "rtmp/flv"
+        }
+      ]
+    };
+  }
+};
+</script>
